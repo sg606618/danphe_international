6e9602bb9f05c08d11b09cae699fb3b03fd9009d
Santosh Ghimire <sg606618@gmail.com>
2024-11-12 09:41:17 +0545

    second phase

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Navbar from "./Navbar";
 
 const header = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // Ensures code only runs on the client side
+  }, []);
+
   return (
     <>
-      <div className="w-100 bg-primary">
-        this is header
+      <div className="w-full bg-[white] px-2 py-3">
+        <div className="w-100 justify-between flex gap-3 max-w-[1366px] mx-auto">
+          <Image
+            className="w-[5rem]"
+            src="/images/danpheinternationallogo.jpg"
+            alt="Danphe International Logo"
+            priority
+            width={200}
+            height={200}
+          />
+          <div className="flex flex-col w-[100%] items-end justify-around">
+            <div className="flex gap-3 text-primary font-bold w-[fit-content]">
+              <p>
+                <a href="mailto: danphe2022@gmail.com">danphe2022@gmail.com</a>
+              </p>
+              <p>
+                <a href="tel: 9841661139">9841661139</a>
+              </p>
+            </div>
+            <Navbar />
+          </div>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default header
+export default header;
--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const navbar = () => {
+  const menuItems = ["HOME", "ABOUT", "SERVICES", "BLOGS", "CONTACT"];
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="flex justify-around text-black font-bold text-lg w-full">
+        {menuItems.map((item) => (
+          <span key={item} className="relative group">
+            <a
+              href={item === "HOME" ? "/" : `/${item.toLowerCase()}`}
+              className="group-hover:text-gray-600"
+            >
+              {item}
+            </a>
+            <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
+          </span>
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default navbar
+export default navbar;
